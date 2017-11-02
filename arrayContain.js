9fdde4c643f83b5d9658175bc5e29592a23fc06c
alanzhang(张伦) <alanzhang@futunn.com>
2017-11-02 15:41:48 +0800

    增加解法

--- arrayContain.js
+++ arrayContain.js
@@ -1,6 +1,7 @@
 var AArray = [2,3,46,75,23],
     BArray = [3,3,46,23];
 
+/*----------------------方法一------------------------------*/
 function contains (A,B) {
     var AStart = 0;
     for (var i = 0,length = B.length;i < length;i++) {
@@ -34,3 +35,32 @@ function contains (A,B) {
 }
 
 console.log("case:", AArray + ["不包含","包含"][~~contains(AArray,BArray)] + BArray);
+
+/*----------------------方法二------------------------------*/
+// 判断A是否包含B
+function compare(A, B) {
+    var indexA = 0,
+        indexB = 0;
+
+    for (var i = 0, length = B.length; i < length;) {
+        if (B[indexB] === A[indexA]) {
+            indexA++;
+            indexB++;
+            i++;
+        } else {
+            indexA++;
+        }
+        if (indexA > A.length) {
+            break;
+        }
+    }
+
+    if (indexB === B.length) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+compare([2, 3, 4, 5, 6, 8, 9], [3, 4, 5, 6, 7, 10]); //true
+compare([2, 3, 4, 5, 6, 8, 9], [3, 4, 5, 6]); //false
